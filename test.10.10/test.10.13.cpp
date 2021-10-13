054b1220cb5ce08de685ceab73c537b6de8b7e2c
竹洋 <greypigeon@zhuyangdeMacBook-Air.local>
2021-10-13 19:49:47 +0800

    不同的二叉树II

---
+++ test.10.10/test.10.13.cpp
@@ -0,0 +1,23 @@
+//不同的二叉搜索树II
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int st,int n) {
+        // 边界条件
+        vector<TreeNode*> ans;
+        if(st > n) return {NULL};   // 一定要返回{NULL}集合，否则上级树根节点i取边界得时候就不能去全排列了，有一个for循环会直接跳过
+        // 递归代码
+        for(int i=st;i<=n;i++){
+            for(auto& l : generateTrees(st,i-1)){
+                for(auto& r : generateTrees(i+1,n)){
+                    TreeNode* root = new TreeNode(i,l,r);
+                    ans.push_back(root);
+                }
+            }            
+        }
+        return ans;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        if(n == 0) return {};
+        return generateTrees(1,n);
+    }
+};
